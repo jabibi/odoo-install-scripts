7e6afe559979a21528c88c43224f0cb574d2073f
jabibi <javimelex@gmail.com>
2016-06-08 08:54:47 +0200

    [FIX] Install python packages as root

--- odoo-install.sh
+++ odoo-install.sh
@@ -23,7 +23,7 @@
 # Odoo
 OE_USER="odoo"
 OE_HOME="/home/$OE_USER"
-OE_HOME_EXT="/home/$OE_USER/git/odoo"
+OE_HOME_EXT="$OE_HOME/git/odoo"
 
 # Enter version for checkout "9.0", "8.0", "7.0", "master" for trunk
 OE_VERSION="8.0"
@@ -31,7 +31,9 @@ OE_VERSION="8.0"
 # Set the superadmin password
 OE_SUPERADMIN="admin"
 OE_CONFIG="$OE_USER-server"
-INIT_FILE=/lib/systemd/system/$OE_CONFIG.service
+
+# Init service
+SYSTEMD_FILE=/lib/systemd/system/$OE_CONFIG.service
 
 # Set locale
 LOCALE_LANG=es_ES.UTF-8
@@ -39,7 +41,9 @@ LOCALE_LANG=es_ES.UTF-8
 #--------------------------------------------------
 # Update Server
 #--------------------------------------------------
-echo -e "\n---- Update Server ----"
+
+sudo echo ""
+echo -e "---- Update Server ----"
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y locales
@@ -47,12 +51,14 @@ sudo apt-get install -y locales
 #--------------------------------------------------
 # Install PostgreSQL Server
 #--------------------------------------------------
+
 sudo dpkg-reconfigure locales
 sudo locale-gen $LOCALE_LANG
 sudo /usr/sbin/update-locale LANG=$LOCALE_LANG
 
 echo -e "\n---- Set locales ----"
-sudo echo "LC_ALL=$LOCALE_LANG" >> /etc/environment
+sudo sed -i "/LC_ALL/d" /etc/environment
+sudo su root -c "echo 'LC_ALL=$LOCALE_LANG' >> /etc/environment"
 
 echo -e "\n---- Install PostgreSQL Server ----"
 sudo apt-get install -y postgresql
@@ -62,6 +68,7 @@ sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
 sudo su - postgres -c "psql -c \"ALTER USER $OE_USER WITH ENCRYPTED PASSWORD '$OE_SUPERADMIN';\""
 
 sudo systemctl restart postgresql.service
+
 #--------------------------------------------------
 # System Settings
 #--------------------------------------------------
@@ -76,6 +83,7 @@ sudo chown $OE_USER:$OE_USER /var/log/$OE_USER
 #--------------------------------------------------
 # Install Basic Dependencies
 #--------------------------------------------------
+
 echo -e "\n---- Install tool packages ----"
 sudo apt-get install -y wget git python-pip python-imaging python-setuptools python-dev libxslt1-dev libxml2-dev libldap2-dev libsasl2-dev node-less postgresql-server-dev-all libjpeg-dev libfreetype6-dev zlib1g-dev libpng12-dev
 
@@ -106,13 +114,16 @@ sudo chown -R $OE_USER:$OE_USER $OE_HOME/*
 #--------------------------------------------------
 # Install Dependencies
 #--------------------------------------------------
+
 echo -e "\n---- Install python packages ----"
-sudo pip install -r $OE_HOME_EXT/requirements.txt
-sudo easy_install pyPdf vatnumber pydot psycogreen suds ofxparse
+sudo su root -c "pip install --upgrade --force-reinstall pip"
+sudo su root -c "pip install -r $OE_HOME_EXT/requirements.txt"
+sudo su root -c "easy_install pyPdf vatnumber pydot psycogreen suds ofxparse"
 
 #--------------------------------------------------
 # Configure ODOO
 #--------------------------------------------------
+
 echo -e "* Create server config file"
 sudo cp $OE_HOME_EXT/debian/openerp-server.conf /etc/$OE_CONFIG.conf
 sudo chown $OE_USER:$OE_USER /etc/$OE_CONFIG.conf
@@ -131,43 +142,40 @@ sudo su root -c "echo 'logfile = /var/log/$OE_USER/$OE_CONFIG$1.log' >> /etc/$OE
 sudo su root -c "echo 'addons_path = $OE_HOME/custom/addons,$OE_HOME_EXT/addons' >> /etc/$OE_CONFIG.conf"
 
 echo -e "* Create startup file"
-sudo su root -c "echo '#!/bin/sh' >> $OE_HOME/start.sh"
-sudo su root -c "echo 'sudo -u $OE_USER $OE_HOME_EXT/odoo.py --config=/etc/$OE_CONFIG.conf' >> $OE_HOME/start.sh"
+sudo su $OE_USER -c "echo '#!/bin/sh' >> $OE_HOME/start.sh"
+sudo su $OE_USER -c "echo 'sudo -u $OE_USER $OE_HOME_EXT/odoo.py --config=/etc/$OE_CONFIG.conf' >> $OE_HOME/start.sh"
 sudo chmod 755 $OE_HOME/start.sh
 
 #--------------------------------------------------
-# Adding ODOO as a deamon (initscript)
+# Adding ODOO as a daemon (initscript)
 #--------------------------------------------------
-sudo rm $INIT_FILE
-sudo touch $INIT_FILE
-sudo chmod 666 $INIT_FILE
+
+sudo rm -f $SYSTEMD_FILE
+sudo touch $SYSTEMD_FILE
+sudo chmod 644 $SYSTEMD_FILE
 
 echo -e "* Create systemd unit file"
-sudo echo '[Unit]' >> $INIT_FILE
-sudo echo 'Description=ODOO Application Server' >> $INIT_FILE
-sudo echo 'Requires=postgresql.service' >> $INIT_FILE
-sudo echo 'After=postgresql.service' >> $INIT_FILE
-sudo echo '[Install]' >> $INIT_FILE
-sudo echo "Alias=$OE_CONFIG.service" >> $INIT_FILE
-sudo echo '[Service]' >> $INIT_FILE
-sudo echo 'Type=simple' >> $INIT_FILE
-sudo echo 'PermissionsStartOnly=true' >> $INIT_FILE
-sudo echo "User=$OE_USER" >> $INIT_FILE
-sudo echo "Group=$OE_USER" >> $INIT_FILE
-sudo echo "SyslogIdentifier=$OE_CONFIG" >> $INIT_FILE
-sudo echo "PIDFile=/run/odoo/$OE_CONFIG.pid" >> $INIT_FILE
-sudo echo "ExecStartPre=/usr/bin/install -d -m755 -o $OE_USER -g $OE_USER /run/odoo" >> $INIT_FILE
-sudo echo "ExecStart=$OE_HOME_EXT/odoo.py -c /etc/$OE_CONFIG.conf --pid=/run/odoo/$OE_CONFIG.pid" >> $INIT_FILE
-sudo echo 'ExecStop=/bin/kill $MAINPID' >> $INIT_FILE
-sudo echo '[Install]' >> $INIT_FILE
-sudo echo 'WantedBy=multi-user.target' >> $INIT_FILE
-
-sudo chmod 644 $INIT_FILE
+sudo su root -c "echo '[Unit]' >> $SYSTEMD_FILE"
+sudo su root -c "echo 'Description=ODOO Application Server' >> $SYSTEMD_FILE"
+sudo su root -c "echo 'Requires=postgresql.service' >> $SYSTEMD_FILE"
+sudo su root -c "echo 'After=postgresql.service' >> $SYSTEMD_FILE"
+sudo su root -c "echo '[Install]' >> $SYSTEMD_FILE"
+sudo su root -c "echo 'Alias=$OE_CONFIG.service' >> $SYSTEMD_FILE"
+sudo su root -c "echo '[Service]' >> $SYSTEMD_FILE"
+sudo su root -c "echo 'Type=simple' >> $SYSTEMD_FILE"
+sudo su root -c "echo 'PermissionsStartOnly=true' >> $SYSTEMD_FILE"
+sudo su root -c "echo 'User=$OE_USER' >> $SYSTEMD_FILE"
+sudo su root -c "echo 'Group=$OE_USER' >> $SYSTEMD_FILE"
+sudo su root -c "echo 'SyslogIdentifier=$OE_CONFIG' >> $SYSTEMD_FILE"
+sudo su root -c "echo 'PIDFile=/run/odoo/$OE_CONFIG.pid' >> $SYSTEMD_FILE"
+sudo su root -c "echo 'ExecStartPre=/usr/bin/install -d -m755 -o $OE_USER -g $OE_USER /run/odoo' >> $SYSTEMD_FILE"
+sudo su root -c "echo 'ExecStart=$OE_HOME_EXT/odoo.py -c /etc/$OE_CONFIG.conf --pid=/run/odoo/$OE_CONFIG.pid' >> $SYSTEMD_FILE"
+sudo su root -c "echo 'ExecStop=/bin/kill $MAINPID' >> $SYSTEMD_FILE"
+sudo su root -c "echo '[Install]' >> $SYSTEMD_FILE"
+sudo su root -c "echo 'WantedBy=multi-user.target' >> $SYSTEMD_FILE"
 
 echo -e "* Enabling Systemd File"
-sudo systemctl enable $INIT_FILE
+sudo systemctl enable $SYSTEMD_FILE
 
 echo -e "-- Starting ODOO Server --"
 sudo systemctl start $OE_CONFIG.service
-
-
