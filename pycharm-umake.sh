cd110e399fe05fe8f76df0879afa73e6d74dba32
Javi Melendez <javimelex@gmail.com>
2017-05-17 11:24:43 +0200

    Create pycharm-umake.sh

---
+++ pycharm-umake.sh
@@ -0,0 +1,44 @@
+
+#!/bin/bash
+################################################################################
+# Script for installing PyCharm on Ubuntu 16.04 (64 bits)
+# Authors
+#   * Javi Melendez, <javimelex@gmail.com> 2016
+#-------------------------------------------------------------------------------
+# USAGE:
+#
+# pycharm-umake
+#
+# EXAMPLE:
+# chmod +x pycharm-umake.sh
+# ./pycharm-umake.sh 
+#
+################################################################################
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+# Install latest stable version of ubuntu-make
+sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make
+sudo apt-get update
+sudo apt-get --reinstall install ubuntu-make
+
+# Install PyCharm Community Edition
+umake ide pycharm
+# or Professional Edition
+#umake ide pycharm-professional
+
+# DONE
+
+# To remove PyCharm installed via umake
+#umake -r ide pycharm
+# or
+#umake -r ide pycharm-professional
+
